811f7a8a8b0e666d14f1a8209a9c6103e656ecb1
Morten <moab@eaaa.dk>
2024-12-02 17:46:38 +0100

    version 1

---
+++ js/javascript.js
@@ -0,0 +1,30 @@
+// Write your self-made javascript code below. Remember to comment
+// on the code. If you use functions, make a short description of 
+// each function. You can use comments to demonstrate
+// your knowledge about programming and JavaScript.
+
+// If you have JavaScript(s) that are shared among various
+// html files, you must reference to that JavaScript in all 
+// relevant html files, like:
+// index.html ---> javascript.js
+// about.html ---> javascript.js
+// products.html ---> javascript.js
+// and so on ...
+
+// If you have JavaScript(s) that are unique for a html file,
+// you must make a reference to that specific JavaScript,
+// like: 
+// index.html --> indexjs.js
+// about.html --> aboutjs.js
+// and so on ...
+
+// You can have JavaScript(s) that are both unique and JavaScript(s)
+// that are to be shared among various html files, like
+
+// index.html ---> javascript.js  (shared)
+// index.html ---> indexjs.js (unique)
+// about.html ---> javascript.js (shared)
+// about.html ---> aboutjs.js (unique)
+
+
+// Let me know if you have question on moab@eaaa.dk
